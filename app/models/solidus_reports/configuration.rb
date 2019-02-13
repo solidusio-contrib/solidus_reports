e91a2443be04dec1446d72318e24e50dc0607679
César Carruitero <ccarruitero@protonmail.com>
2019-02-12 23:21:33 -0500

    add conditional to reports menu tab

--- app/models/solidus_reports/configuration.rb
+++ app/models/solidus_reports/configuration.rb
@@ -2,7 +2,11 @@ module SolidusReports
   class Configuration < Spree::Preferences::Configuration
     REPORT_TABS ||= [:reports]
 
-    new_item = Spree::BackendConfiguration::MenuItem.new(REPORT_TABS, 'file')
+    new_item = Spree::BackendConfiguration::MenuItem.new(
+      REPORT_TABS,
+      'file',
+      condition: -> { can?(:admin, REPORT_TABS) }
+    )
     Spree::Backend::Config.menu_items << new_item
   end
 end
