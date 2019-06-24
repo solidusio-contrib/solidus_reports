e95de366840d73cc2b627908416fa71f24b1c7e6
Alessandro Desantis <desa.alessandro@gmail.com>
2019-06-24 10:55:44 +0200

    Fix Reports tab appearing for logged out users

--- lib/solidus_reports/configuration.rb
+++ lib/solidus_reports/configuration.rb
@@ -2,7 +2,12 @@ module SolidusReports
   class Configuration < Spree::Preferences::Configuration
     REPORT_TABS ||= [:reports]
 
-    new_item = Spree::BackendConfiguration::MenuItem.new(REPORT_TABS, 'file')
+    new_item = Spree::BackendConfiguration::MenuItem.new(
+      REPORT_TABS,
+      'file',
+      condition: -> { current_spree_user && current_spree_user.admin? },
+    )
+
     Spree::Backend::Config.menu_items << new_item
   end
 end
