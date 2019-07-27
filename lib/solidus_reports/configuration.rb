b38876a2c93ba031ab956752ab0935eeb56edf34
Memo Toro <memo.toro@gmail.com>
2019-07-27 19:58:38 +0100

    Adding condition for rendering MenuItem

--- lib/solidus_reports/configuration.rb
+++ lib/solidus_reports/configuration.rb
@@ -2,7 +2,7 @@ module SolidusReports
   class Configuration < Spree::Preferences::Configuration
     REPORT_TABS ||= [:reports]
 
-    new_item = Spree::BackendConfiguration::MenuItem.new(REPORT_TABS, 'file')
+    new_item = Spree::BackendConfiguration::MenuItem.new(REPORT_TABS, 'file', condition: -> { can?(:admin, :reports) })
     Spree::Backend::Config.menu_items << new_item
   end
 end
