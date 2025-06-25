f037a32b184011fe4608309a6c72d304f98e692b
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2025-06-25 10:02:07 +0200

    Fix Deprecation warning

--- lib/solidus_reports/engine.rb
+++ lib/solidus_reports/engine.rb
@@ -17,13 +17,12 @@ module SolidusReports
     end
 
     initializer "solidus_reports.environment", before: :load_config_initializers do
-      REPORT_TABS ||= [:reports].freeze
-
-      new_item = Spree::BackendConfiguration::MenuItem.new(
-        REPORT_TABS,
-        'file',
+      new_item = Spree::Backend::Config.class::MenuItem.new(
+        label: :reports,
+        icon: 'file',
         condition: -> { can?(:admin, :reports) }
       )
+
       Spree::Backend::Config.menu_items << new_item
     end
   end
